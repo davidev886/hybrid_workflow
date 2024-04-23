b04a9eba66176fc6904ec769ea1d104fea636e5e
Davide Vodola <davide.vodola@basf.com>
2024-04-23 14:29:19 +0200

    Added unpacking

--- prepare_ipie_input.py
+++ prepare_ipie_input.py
@@ -6,10 +6,10 @@ from pyscf import gto, scf, fci, mcscf, lib
 from pyscf.lib import chkfile
 import shutil
 import h5py
-from ipie.hamiltonians.generic import Generic as HamGeneric
-from ipie.qmc.afqmc import AFQMC
-from ipie.systems.generic import Generic
-from ipie.trial_wavefunction.particle_hole import ParticleHoleNonChunked
+# from ipie.hamiltonians.generic import Generic as HamGeneric
+# from ipie.qmc.afqmc import AFQMC
+# from ipie.systems.generic import Generic
+# from ipie.trial_wavefunction.particle_hole import ParticleHoleNonChunked
 from src.from_pyscf_mod import gen_ipie_input_from_pyscf_chk_mod
 
 
@@ -121,6 +121,48 @@ if __name__ == "__main__":
     from ipie.utils.chunk_large_chol import split_cholesky
 
     split_cholesky(os.path.join(ipie_input_dir, ham_file), num_gpus)  # split the cholesky to 4 subfiles
+
+    from mpi4py import MPI
+    from ipie.utils.mpi import MPIHandler, make_splits_displacements
+    from ipie.utils.pack_numba import pack_cholesky
+    nmembers = 1
+    comm = MPI.COMM_WORLD
+    num_walkers = 24 // comm.size
+    nsteps = 25
+    nblocks = 4
+    timestep = 0.005
+    rng_seed = None
+
+    with h5py.File(os.path.join(ipie_input_dir, ham_file)) as fa:
+        e0 = fa["e0"][()]
+        hcore = fa["hcore"][()]
+
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    srank = rank % nmembers
+
+    handler = MPIHandler(nmembers=nmembers, verbose=True)
+
+    num_basis = hcore.shape[-1]
+    with h5py.File(f"chol_{srank}.h5") as fa:
+        chol_chunk = fa["chol"][()]
+
+    chunked_chols = chol_chunk.shape[-1]
+    num_chol = handler.scomm.allreduce(chunked_chols, op=MPI.SUM)
+
+    chol_chunk_view = chol_chunk.reshape((num_basis, num_basis, -1))
+    cp_shape = (num_basis * (num_basis + 1) // 2, chol_chunk_view.shape[-1])
+    chol_packed_chunk = np.zeros(cp_shape, dtype=chol_chunk_view.dtype)
+    sym_idx = np.triu_indices(num_basis)
+    pack_cholesky(sym_idx[0], sym_idx[1], chol_packed_chunk, chol_chunk_view)
+    del chol_chunk_view
+
+    split_size = make_splits_displacements(num_chol, nmembers)[0]
+    assert chunked_chols == split_size[srank]
+
+
+
+
 
     exit()
     with h5py.File(os.path.join(ipie_input_dir, ham_file)) as fa:
